f65c5fac1febcc1cd7d27ac8a709f45cabd272a9
ajlyon <ajlyon@eec034f8-935b-11dd-aed7-ed23b0274063>
2008-10-09 11:26:56 +0000

    Prefix length was too short. Changed to 20 chars.

--- structure.sql
+++ structure.sql
@@ -3,7 +3,7 @@
 CREATE TABLE `tournaments` (
       `id` int(11) NOT NULL auto_increment,
       `name` varchar(45) NOT NULL,
-      `prefix` varchar(10) NOT NULL,
+      `prefix` varchar(20) NOT NULL,
       `username` varchar(30) NOT NULL,
       `password` varchar(30) NOT NULL,
       `locked` tinyint(1) NOT NULL default '0',
