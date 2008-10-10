dd0635e82fb0ff7c7e9352a7806abb47d4e693e8
ajlyon <ajlyon@eec034f8-935b-11dd-aed7-ed23b0274063>
2008-10-10 00:30:23 +0000

    Added points per game, added rounding.

--- stats_individual.php
+++ stats_individual.php
@@ -10,18 +10,29 @@
  $title="Individual statistics";
  require "head.php";			// Generate header as appropriate
 
-     $res1 = query("SELECT CONCAT("."$mysql_prefix"."_players.first_name,\" \","."$mysql_prefix"."_players.last_name),
+ $res1 = query("SELECT 
+                            CONCAT("."$mysql_prefix"."_players.first_name,\" \","."$mysql_prefix"."_players.last_name),
 			    "."$mysql_prefix"."_teams.short_name,
 			    SUM(IF("."$mysql_prefix"."_players.id=player_id,powers,0)) AS pow,
 			    SUM(IF("."$mysql_prefix"."_players.id=player_id,tossups,0)) AS tu,
 			    SUM(IF("."$mysql_prefix"."_players.id=player_id,negs,0)) AS neg,
 			    SUM(IF("."$mysql_prefix"."_players.id=player_id,powers,0)*15+IF("."$mysql_prefix"."_players.id=player_id,tossups,0)*10-IF("."$mysql_prefix"."_players.id=player_id,negs,0)*5) as tp,
-			    SUM(IF("."$mysql_prefix"."_players.id=player_id,powers,0)*15+IF("."$mysql_prefix"."_players.id=player_id,tossups,0)*10-IF("."$mysql_prefix"."_players.id=player_id,negs,0)*5)/SUM(IF("."$mysql_prefix"."_players.id=player_id,tu_heard,0)) AS pptuh,
-			    SUM(IF("."$mysql_prefix"."_players.id=player_id,powers,0))/SUM(IF("."$mysql_prefix"."_players.id=player_id,negs,0)) as pn,
+                            FORMAT(SUM(IF("."$mysql_prefix"."_players.id=player_id,powers,0)*15+IF("."$mysql_prefix"."_players.id=player_id,tossups,0)*10-IF("."$mysql_prefix"."_players.id=player_id,negs,0)*5)/SUM(IF("."$mysql_prefix"."_players.id=player_id,tu_heard,0)),2) AS pptuh,
+                            FORMAT(SUM(IF("."$mysql_prefix"."_players.id=player_id,powers,0)*15+IF("."$mysql_prefix"."_players.id=player_id,tossups,0)*10-IF("."$mysql_prefix"."_players.id=player_id,negs,0)*5)/SUM(IF("."$mysql_prefix"."_players.id=player_id,tu_heard,0))*{$tourney_game_length},2) AS ppg,
+			    FORMAT(SUM(IF("."$mysql_prefix"."_players.id=player_id,powers,0))/SUM(IF("."$mysql_prefix"."_players.id=player_id,negs,0)),2) as pn,
 			    SUM(IF("."$mysql_prefix"."_players.id=player_id,tu_heard,0)) AS tuh
-				FROM "."$mysql_prefix"."_players,"."$mysql_prefix"."_rounds_players,"."$mysql_prefix"."_teams WHERE "."$mysql_prefix"."_rounds_players.team_id="."$mysql_prefix"."_teams.id AND "."$mysql_prefix"."_players.id="."$mysql_prefix"."_rounds_players.player_id GROUP BY player_id ORDER BY pptuh DESC");
-     table($res1,array("Name","Team","15","10","-5","Pts.","PPTUH","P/N","TUH"),9,TRUE,FALSE,"stats");
-     free_result($res1);
+                            FROM "."$mysql_prefix"."_players,
+                                "."$mysql_prefix"."_rounds_players,
+                                "."$mysql_prefix"."_teams
+                            WHERE "."$mysql_prefix"."_rounds_players.team_id="."$mysql_prefix"."_teams.id 
+                                 AND "."$mysql_prefix"."_players.id="."$mysql_prefix"."_rounds_players.player_id
+                            GROUP BY player_id ORDER BY pptuh DESC") or die(mysql_error());
+     table($res1,array("Name","Team","15","10","-5","Pts.","PPTUH","PPG","P/N","TUH"),9,TRUE,FALSE,"stats");
+ free_result($res1);
+?>
+    <p><strong>Note:</strong> The PPG above is calculated on the basis of tossups heard, 
+not number of games played. (<?=$tourney_game_length?> tossups per game)</p>
+<?php
  
  require "foot.php";			// finish off page
 ?>
