178fbc81f53dc759f3fefbf90fb02b2f83e3c32d
ajlyon <ajlyon@eec034f8-935b-11dd-aed7-ed23b0274063>
2008-10-10 02:21:34 +0000

    Added options field to table(..) function, to let tables be ranked. Ranking still not perfect-- doesn't handle ties correctly.

--- trunk/rosters.php
+++ trunk/rosters.php
@@ -31,7 +31,7 @@
                             AND {$mysql_prefix}_teams.id = {$mysql_prefix}_players.team 
 			GROUP BY {$mysql_prefix}_players.id
 			ORDER BY pptuh DESC");
-     table($res1,array("Name","15","10","-5","Pts.","PPTUH","P/N","TUH"),8,TRUE,FALSE,"stats");
+     table($res1,array("Name","15","10","-5","Pts.","PPTUH","P/N","TUH"),8,TRUE,FALSE,"stats",array());
  }
      free_result($res1);
  
