eb6f06be67b1c3f44098fb1355b77da0f75b7b1c
ajlyon <ajlyon@eec034f8-935b-11dd-aed7-ed23b0274063>
2009-01-12 02:09:55 +0000

    Changes to interface for login.

--- trunk/index.php
+++ trunk/index.php
@@ -12,14 +12,22 @@ $title = $tourney_name;
 require "head.php";			// Generate header as appropriate
 ?>
     <p id="description"><?=$tourney_desc?></p>
+<div id="login">
 <? if(!$auth) { ?>
 <form method="post" action="?login&t=<?=$mysql_prefix?>">
- <p><input type="text" name="login_u" size="10" /><input type="password" name="login_p" size="10" /> <input type="submit" value="Log in" /></p>
+ <h3>Login</h3>
+ <p>Username: <input type="text" name="login_u" size="10" /> Password: <input type="password" name="login_p" size="10" /> <input type="submit" value="Log in" /></p>
  </form>
 <? } else {?>
-<p><a href="?kill=now&t=<?=$mysql_prefix?>">[Log out]</a></p>     
- <?
+<form action="?" method="get">
+<p>Currently logged in to tournament.
+<input type="submit" value="Log out" />
+<input type="hidden" name="kill" value="now" />
+<input type="hidden" name="t" value="<?=$mysql_prefix?>" /></p></form>     
+ <?php
 }
+?>
+</div>
+<?php
  require "foot.php";			// finish off page
-
  ?>
