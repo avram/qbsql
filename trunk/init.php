cc3d8dcb2e081be95fd16638621bc838cf877485
ajlyon <ajlyon@eec034f8-935b-11dd-aed7-ed23b0274063>
2009-02-06 12:16:59 +0000

    First migration test. The new config file is needed as well.

--- trunk/init.php
+++ trunk/init.php
@@ -10,7 +10,7 @@
 
     // If we have a database migration, do it.
     /* Minimum db revision for this version of the code */
-    $required_rev = 36;
+    $required_rev = 37;
     require_once("migrations.php");
     migrate($required_rev);
 
