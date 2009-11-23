62391e0c123facb60bcbe089c9a41e0444bf249a
majj@stanford.edu <majj@stanford.edu@eec034f8-935b-11dd-aed7-ed23b0274063>
2009-11-23 18:47:34 +0000

    Included proper forfeit checks in round summaries.

--- stats_round.php
+++ stats_round.php
@@ -16,19 +16,27 @@ $js_includes = true;
  
  $res1 = query("SELECT {$mysql_prefix}_rounds.id,
 			    CONCAT('<a href=\"stats_team.php?t={$mysql_prefix}&team=',
-			         IF(score1>=score2,{$mysql_prefix}_tut1.team_id,
+			         IF(((score1>=score2 OR forfeit = {$mysql_prefix}_tut2.team_id)
+			         		AND forfeit!={$mysql_prefix}_tut1.team_id),{$mysql_prefix}_tut1.team_id,
 			                         {$mysql_prefix}_tut2.team_id), '\">', 
-			         IF(score1>=score2,t1.full_name,t2.full_name),
+			         IF(((score1>=score2 OR forfeit = {$mysql_prefix}_tut2.team_id)
+			         		AND forfeit!={$mysql_prefix}_tut1.team_id),t1.full_name,t2.full_name),
 			     '</a>') AS name1,
-			    IF(score1>=score2,score1,score2) AS winscore,
-			    FORMAT(IF(score1>=score2,(score1-{$mysql_prefix}_tut1.tup)/{$mysql_prefix}_tut1.tuc,(score2-{$mysql_prefix}_tut2.tup)/{$mysql_prefix}_tut2.tuc),2) AS winconv,
+			    IF(((score1>=score2 OR forfeit = {$mysql_prefix}_tut2.team_id)
+			         		AND forfeit!={$mysql_prefix}_tut1.team_id),score1,score2) AS winscore,
+			    FORMAT(IF(((score1>=score2 OR forfeit = {$mysql_prefix}_tut2.team_id)
+			         		AND forfeit!={$mysql_prefix}_tut1.team_id),(score1-{$mysql_prefix}_tut1.tup)/{$mysql_prefix}_tut1.tuc,(score2-{$mysql_prefix}_tut2.tup)/{$mysql_prefix}_tut2.tuc),2) AS winconv,
 			    CONCAT('<a href=\"stats_team.php?t={$mysql_prefix}&team=',
-			         IF(score1>=score2,{$mysql_prefix}_tut2.team_id,
+			         IF(((score1>=score2 OR forfeit = {$mysql_prefix}_tut2.team_id)
+			         		AND forfeit!={$mysql_prefix}_tut1.team_id),{$mysql_prefix}_tut2.team_id,
 			                         {$mysql_prefix}_tut1.team_id), '\">', 
-			         IF(score1>=score2,t2.full_name,t1.full_name),
+			         IF(((score1>=score2 OR forfeit = {$mysql_prefix}_tut2.team_id)
+			         		AND forfeit!={$mysql_prefix}_tut1.team_id),t2.full_name,t1.full_name),
 			     '</a>') AS name2,
-			    IF(score1>=score2,score2,score1) AS losescore,
-			    FORMAT(IF(score1>=score2,(score2-{$mysql_prefix}_tut2.tup)/{$mysql_prefix}_tut2.tuc,(score1-{$mysql_prefix}_tut1.tup)/{$mysql_prefix}_tut1.tuc),2) AS loseconv,
+			    IF(((score1>=score2 OR forfeit = {$mysql_prefix}_tut2.team_id)
+			         		AND forfeit!={$mysql_prefix}_tut1.team_id),score2,score1) AS losescore,
+			    FORMAT(IF(((score1>=score2 OR forfeit = {$mysql_prefix}_tut2.team_id)
+			         		AND forfeit!={$mysql_prefix}_tut1.team_id),(score2-{$mysql_prefix}_tut2.tup)/{$mysql_prefix}_tut2.tuc,(score1-{$mysql_prefix}_tut1.tup)/{$mysql_prefix}_tut1.tuc),2) AS loseconv,
 			    ABS(score1-score2) $detail $edit_query
 			FROM
                         (
