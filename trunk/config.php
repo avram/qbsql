0549168b60aace381aa37b1aea10cc808c933c02
ajlyon <ajlyon@eec034f8-935b-11dd-aed7-ed23b0274063>
2009-12-03 08:07:49 +0000

    Removing wymeditor examples directory and fixing config regression.

--- trunk/config.php
+++ trunk/config.php
@@ -8,10 +8,10 @@ $master_username = "root";
 $master_password = "123456";
 
 // These three lines should be changed to reflect your MySQL configuration.
-$mysql_host = "db1865.perfora.net";
-$mysql_username = "dbo273545831";
-$mysql_pass = "tr00dnamne";
-$mysql_db = "db273545831";
+$mysql_host = "localhost";
+$mysql_username = "my_username";
+$mysql_pass = "my_password";
+$mysql_db = "my_database";
 
 // Notification
 $debug = TRUE;
