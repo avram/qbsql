8da6a99b3029ce6bf8db78f64ebcda56e8fa03fa
ajlyon <ajlyon@eec034f8-935b-11dd-aed7-ed23b0274063>
2010-02-14 21:13:06 +0000

    Fixes Rounds summary display's handling of multiple games for a single team during one round (i.e., if imported from SQBS with bad data); now should show correct BConv.
    Also adds BConv checking at SQL level (half of issue 36), and some UI improvement (link to newly created/edited game after editing/adding)

--- trunk/stats_round.php
+++ trunk/stats_round.php
@@ -50,24 +50,24 @@ $js_includes = true;
 		    SUM(powers+tossups) as tuc,
 		    SUM(powers) as pow,
 		    SUM(negs) AS neg,
-		    team_id, round_id 
-                    from {$mysql_prefix}_teams,{$mysql_prefix}_rounds_players where {$mysql_prefix}_teams.id={$mysql_prefix}_rounds_players.team_id group by round_id,team_id
+		    team_id, game_id 
+                    from {$mysql_prefix}_teams,{$mysql_prefix}_rounds_players where {$mysql_prefix}_teams.id={$mysql_prefix}_rounds_players.team_id group by game_id,team_id
                             ) AS {$mysql_prefix}_tut1, 
                         (
 		    select SUM(powers*15+tossups*10+negs*(-5)) as tup,
 		    SUM(powers+tossups) as tuc,
 		    SUM(powers) as pow,
 		    SUM(negs) AS neg,
-		    team_id, round_id 
-                    from {$mysql_prefix}_teams,{$mysql_prefix}_rounds_players where {$mysql_prefix}_teams.id={$mysql_prefix}_rounds_players.team_id group by round_id,team_id
+		    team_id, game_id 
+                    from {$mysql_prefix}_teams,{$mysql_prefix}_rounds_players where {$mysql_prefix}_teams.id={$mysql_prefix}_rounds_players.team_id group by game_id,team_id
                             ) AS {$mysql_prefix}_tut2,
 			    {$mysql_prefix}_rounds, 
 			    {$mysql_prefix}_teams AS t1, {$mysql_prefix}_teams AS t2
 			WHERE t1.id={$mysql_prefix}_rounds.team1
 			    AND t2.id={$mysql_prefix}_rounds.team2
-			    AND {$mysql_prefix}_tut1.round_id={$mysql_prefix}_rounds.id
+			    AND {$mysql_prefix}_tut1.game_id={$mysql_prefix}_rounds.game_id
 			    AND {$mysql_prefix}_tut1.team_id=t1.id
-			    AND {$mysql_prefix}_tut2.round_id={$mysql_prefix}_rounds.id
+			    AND {$mysql_prefix}_tut2.game_id={$mysql_prefix}_rounds.game_id
 			    AND {$mysql_prefix}_tut2.team_id=t2.id
 			ORDER BY {$mysql_prefix}_rounds.id ASC") or die(mysql_error());
      table($res1,array("Round","W","","BConv","L","","BConv","Margin","Detail"),9,TRUE,FALSE,"stats",array("sort"));
